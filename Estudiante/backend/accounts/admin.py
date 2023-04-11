25f44c4c5ce360df53311116bd53365b01c471cb
Danileo10 <dlozano436@gmail.com>
2023-04-11 13:59:04 -0500

    base de datos Estudiante creada

--- Estudiante/backend/accounts/admin.py
+++ Estudiante/backend/accounts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Estudiante, Perfil, Rol
 
-# Register your models here.
+class EstudianteAdmin(admin.ModelAdmin):
+    list_display = ("codigo","nombre","apellido", "telefono")
+    
+class PerfilAdmin(admin.ModelAdmin):
+    list_display = ("idPerfil","usuario","correo", "contraseña","tipo_documento","documento")
+
+class RolAdmin(admin.ModelAdmin):
+    list_display = ("idRol","nombre_rol")
+
+admin.site.register(Estudiante, EstudianteAdmin)
+admin.site.register(Perfil, PerfilAdmin)
+admin.site.register(Rol, RolAdmin)
--- Estudiante/backend/accounts/models.py
+++ Estudiante/backend/accounts/models.py
@@ -1,3 +1,35 @@
 from django.db import models
-from 
-# Create your models here.
+
+
+
+class Rol(models.Model):
+    idRol = models.AutoField(primary_key=True)
+    nombre_rol = models.CharField(max_length=80)
+
+    def __str__(self):
+        return self.idRol
+
+class Perfil(models.Model):
+    idPerfil = models.AutoField(primary_key=True)
+    usuario = models.CharField(max_length=80)
+    correo = models.CharField(max_length=80)
+    contraseña = models.CharField(max_length=80)
+    tipo_documento = models.CharField(max_length=45)
+    documento = models.CharField(max_length=20)
+    idRol = models.ForeignKey(Rol, null=False, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.idPerfil
+     
+class Estudiante(models.Model):
+    codigo = models.IntegerField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=100)
+    idPerfil = models.ForeignKey(Perfil, null=False, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.codigo
+
+
+
